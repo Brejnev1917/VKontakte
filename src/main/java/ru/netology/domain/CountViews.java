2dce0878734a94842a6e201a843e65ace877d1e1
brejnev1917 <brejnev1917@gmail.com>
2022-02-02 17:27:46 +0300

    Second commit

---
+++ src/main/java/ru/netology/domain/CountViews.java
@@ -0,0 +1,5 @@
+package ru.netology.domain;
+
+public class CountViews {
+    private int count;
+}
---
+++ src/main/java/ru/netology/domain/DonatInfo.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class DonatInfo {
+    private boolean isDonat;
+    private int paidDuration;
+    private boolean canPublishFreeCopy;
+    private String editMode;
+}
